e09e39346b7cad3f1aa124c370252a53567c2033
Ghost <62175028+zororai@users.noreply.github.com>
2026-07-08 11:07:40 +0200

    Rename project refs, update env and add nvm
    Align local dev configuration and project paths after renaming to 'my-code'. Changes include: update built asset paths in public/js/app.js to the new project folder, update .env settings (APP_KEY, APP_URL, DB_DATABASE) without exposing secrets, add .nvmrc to pin Node to 26, add git helper Bash commands in .claude/settings.local.json, and remove the public/storage directory.

--- public/js/app.js
+++ public/js/app.js
@@ -40667,8 +40667,8 @@ try {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Mazarura\Documents\GitHub\roshs\student_portal_roshs\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Mazarura\Documents\GitHub\roshs\student_portal_roshs\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Mazarura\Documents\GitHub\my-code\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Mazarura\Documents\GitHub\my-code\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
